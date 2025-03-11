43bedfe518cfebfa5d8935a7b01c787f4b61d3f3
DESKTOP-SH3SALR\Wetalk-PC <gustavo@wetalkit.com.br>
2025-03-11 17:47:16 -0300

    adicionei o teste para prever se caso o usuário colocar um senha inválida

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -10,7 +10,7 @@ app.use("/auth", routerAuth);
 describe("Testando rota de criação de usuário", () => {
   it("O usuário não pode ser criado, pois o email não é válido", async () => {
     const response = await request(app)
-    .post("/auth/create")
+      .post("/auth/create")
       .send({
         userName: "Lucasss",
         email: "1234456775",
@@ -19,8 +19,29 @@ describe("Testando rota de criação de usuário", () => {
       .expect("Content-Type", /json/)
       .expect(400);
 
-      expect(response.body).toHaveProperty("message");
-      expect(Array.isArray(response.body.message)).toBe(true);
-      expect(response.body.message).toContainEqual({ message: "Email inválido" });
+    expect(response.body).toHaveProperty("message");
+    expect(Array.isArray(response.body.message)).toBe(true);
+    expect(response.body.message).toContainEqual({ message: "Email inválido" });
+  });
+});
+
+describe("Testando rota de criação de usuário", () => {
+  it("O usuário não pode ser criado, pois a senha não é válida", async () => {
+    const response = await request(app)
+      .post("/auth/create")
+      .send({
+        userName: "Lucasss",
+        email: "gustavo@fuffuf.com.br",
+        password: "123345",
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toHaveProperty("message");
+    expect(Array.isArray(response.body.message)).toBe(true);
+    expect(response.body.message).toEqual([
+      { message: "A senha deve ter pelo menos 8 caracteres" },
+      { message: "A senha deve conter pelo menos um caractere especial e uma letra maiúscula" }
+    ]);
   });
 });
