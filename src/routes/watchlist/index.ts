0f5cb6499fd22f8cd520fffdee63692d1ef71720
DESKTOP-SH3SALR\Wetalk-PC <gustavo@wetalkit.com.br>
2025-03-18 11:45:55 -0300

    fiz a adição da rota de deletar livro da watchlist do usuário

--- src/routes/watchlist/index.ts
+++ src/routes/watchlist/index.ts
@@ -8,52 +8,50 @@ import { z } from "zod";
 export const routerWatchlist = Router();
 
 routerWatchlist.post(
-    "/create/:bookId",
-    validateJWT,
-    async (req, res): Promise<void> => {
-      const bookId = Number(req.params.bookId);
-      const userData = await extractUserDataFromToken(req);
-  
-      try {
-        const bookExists = await prisma.books.findUnique({
-          where: { id: bookId },
-        });
-        if (!bookExists) {
-          res.status(400).json({ message: "Livro não se encontra no sistema" });
-          return;
-        }
-  
-        const alreadyInWatchlist = await prisma.watchlist.findFirst({
-          where: {
-            user_id: userData.id,
-            book_id: bookId,
-          },
-        });
-  
-        if (alreadyInWatchlist) {
-          res
-            .status(409)
-            .json({ message: "Livro já se encontra na sua watchlist" });
-          return;
-        }
-  
-        await prisma.watchlist.create({
-          data: {
-            user_id: userData.id,
-            book_id: bookId,
-          },
-        });
-  
-        res
-          .status(201)
-          .json({ message: "Watchlist criada com sucesso para o usuário" });
-      } catch (error) {
-        console.error("Erro durante a criação da watchlist:", error);
-        res.status(500).json({ message: "Erro interno do servidor." });
+  "/create/:bookId",
+  validateJWT,
+  async (req, res): Promise<void> => {
+    const bookId = Number(req.params.bookId);
+    const userData = await extractUserDataFromToken(req);
+
+    try {
+      const exists = await verifyBookExists(bookId);
+      if (!exists) {
+        res.status(400).json({ message: "Livro não se encontra no sistema" });
+        return;
       }
+
+      const alreadyInWatchlist = await prisma.watchlist.findFirst({
+        where: {
+          user_id: userData.id,
+          book_id: bookId,
+        },
+      });
+
+      if (alreadyInWatchlist) {
+        res
+          .status(409)
+          .json({ message: "Livro já se encontra na sua watchlist" });
+        return;
+      }
+
+      await prisma.watchlist.create({
+        data: {
+          user_id: userData.id,
+          book_id: bookId,
+        },
+      });
+
+      res
+        .status(201)
+        .json({ message: "Watchlist criada com sucesso para o usuário" });
+    } catch (error) {
+      console.error("Erro durante a criação da watchlist:", error);
+      res.status(500).json({ message: "Erro interno do servidor." });
     }
-  );
-  
+  }
+);
+
 routerWatchlist.get(
   "/user/list",
   validateJWT,
@@ -86,3 +84,39 @@ routerWatchlist.get(
     }
   }
 );
+
+routerWatchlist.delete(
+  "/delete/:bookId",
+  validateJWT,
+  async (req, res): Promise<void> => {
+    const bookId = Number(req.params.bookId);
+    const userData = await extractUserDataFromToken(req);
+    try {
+      const exists = await verifyBookExists(bookId);
+      if (!exists) {
+        res.status(400).json({ message: "Livro não se encontra no sistema" });
+        return;
+      }
+
+      await prisma.watchlist.delete({
+        where: {
+          user_id_book_id: {
+            user_id: userData.id,
+            book_id: bookId,
+          },
+        },
+      });
+      res.status(200).json({ message: "Watchlist deletada com sucesso" });
+    } catch (error) {
+      console.error("Erro durante a exclusão da watchlist:", error);
+      res.status(500).json({ message: "Erro interno do servidor." });
+    }
+  }
+);
+
+async function verifyBookExists(id: number): Promise<boolean> {
+  const book = await prisma.books.findUnique({
+    where: { id },
+  });
+  return book !== null;
+}
